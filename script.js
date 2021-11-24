52730d1723a60925a1c1691ed2ef0514726d6501
ssmeds <stina_smeds@hotmail.com>
2021-11-24 12:58:43 +0100

    cleaned up the code and styling

--- script.js
+++ script.js
@@ -1,130 +1,74 @@
-// 'use strict'
-
-// fetch('./img.json')
-//     .then(response => response.json())
-//     .then(data => {
-
-//         const jsonImages = data;
-//         let imagesDiv = document.querySelector('.imageContainer')
-//         console.log(imagesDiv);
-//         jsonImages.forEach((image) => {
-//             imagesDiv.innerHTML += '<img class="images" src="' + image.url + '"id="' + image.id + '"alt="' + image.alt + '"title="' + image.title + '"id="' + image.id + '"/>';
-//         })
-//         document.querySelectorAll('.imageContainer img').forEach(image => {
-//             image.onclick = () => {
-//                 console.log('hej');
-//                 document.querySelector('.popupImage').style.display = 'block';
-//                 document.querySelector('.popupImage img').src = image.getAttribute('src')
-//             }
-//         })
-//         document.querySelector('.popupImage span').onclick = () => {
-//             document.querySelector('.popupImage').style.display = 'none';
-//         }
-//         document.addEventListener('keydown', function (event) {
-//             const key = event.key; // const {key} = event; in ES6+
-//             if (key === "Escape") {
-//                 document.querySelector('.popupImage').style.display = 'none';
-//             }
-//         });
-
-//     })
 'use strict'
+///////////////// Getting and creating all elements //////////////////////////
+let imagesDiv = document.querySelector('.imageContainer')
+const popupImageDiv = document.querySelector('.popupImage')
+const popupImage = document.querySelector('.popupImage img')
+const span = document.querySelector('.popupImage span')
+const buttons = document.querySelector('.buttons')
 
+let descDiv = document.createElement('div')
+popupImage.insertAdjacentElement('afterend', descDiv)
+descDiv.classList.add('descDiv')
+
+///////////////// Fetching data /////////////////////////////////////////////
 fetch('./img.json')
     .then(response => response.json())
     .then(data => {
-
         const jsonImages = data;
-        let imagesDiv = document.querySelector('.imageContainer')
-        console.log(imagesDiv);
         jsonImages.forEach((image) => {
-            imagesDiv.innerHTML += `<img id=${image.id} class="images" src=${image.url} alt="${image.alt}"/>`
-
-            // ${image.description}${image.title} 
-
+            imagesDiv.innerHTML += `<img id=${image.id} class="images" src=${image.url} alt="${image.alt}" data-src="${image.url}"/>`
         })
-        let descDiv = document.createElement('div')
+
         document.querySelectorAll('.imageContainer img').forEach(image => {
-            console.log(image);
-            image.onclick = () => {
-                console.log('hej');
-                console.log(image.id);
-
-
-                document.querySelector('.popupImage').style.display = 'block';
-                document.querySelector('.popupImage img').src = image.getAttribute('src')
-                document.querySelector('.popupImage').appendChild(descDiv)
-                descDiv.classList.add('descDiv')
-                showText(image.id)
-
-
-
+            image.onclick = (e) => {
+                let clickedImgId = e.target.id
+                popupImageDiv.style.display = 'block';
+                popupImage.src = image.getAttribute('src')
+                let indexOfImg = jsonImages.findIndex(images => images.id === clickedImgId);
+                setImg(indexOfImg)
             }
         })
-        document.querySelector('.popupImage span').onclick = () => {
-            document.querySelector('.popupImage').style.display = 'none';
+
+        ////////////////// Event listeners //////////////////
+        span.onclick = () => {
+            popupImageDiv.style.display = 'none';
         }
-        document.addEventListener('keydown', function (event) {
+
+        document.addEventListener('keydown', (event) => {
             const key = event.key; // const {key} = event; in ES6+
             if (key === "Escape") {
-                document.querySelector('.popupImage').style.display = 'none';
+                popupImageDiv.style.display = 'none';
             }
         });
-        const popupImage = document.querySelector('.popupImage img')
-        const btnPrev = document.querySelector('.btnPrev')
-        const btnNext = document.querySelector('.btnNext')
-        const imgArray = []
 
-        jsonImages.forEach(images => {
-            imgArray.push(images.url)
-        });
-        btnPrev.addEventListener('click', (e) => prev(e))
-        btnNext.addEventListener('click', (e) => next(e))
+        buttons.addEventListener('click', (e) => move(e))
 
-        function prev(e) {
-            console.log("e", e);
-            console.log(e.path[2].children[1].currentSrc);
+        /////////////////// Functions ///////////////////////
+        const move = (e) => {
+            let buttonClass = e.target.className;
+            const imageUrl = e.path[2].children[1].attributes[0].value;
+            let indexOfImg = jsonImages.findIndex(image => image.url === imageUrl);
 
-            const imageUrl = e.path[2].children[1].currentSrc;
-            const sliced = imageUrl.slice(22);
-            console.log("sliced", sliced);
-            let indexOfImg = imgArray.findIndex(image => image === sliced);
-            console.log("imgArray", imgArray);
-            console.log("index", indexOfImg);
-            if (indexOfImg <= 0) indexOfImg = imgArray.length;
-            indexOfImg--;
-            return setImg(indexOfImg)
+            if (buttonClass === 'btnPrev') {
+                if (indexOfImg <= 0) {
+                    indexOfImg = jsonImages.length - 1
+                    return setImg(indexOfImg)
+                }
+                indexOfImg--;
+                return setImg(indexOfImg)
+
+            } else if (buttonClass == 'btnNext') {
+                if (indexOfImg >= jsonImages.length - 1) {
+                    indexOfImg = 0
+                    return setImg(indexOfImg)
+                }
+                indexOfImg++;
+                return setImg(indexOfImg)
+            }
         }
 
-        function next(e) {
-            console.log("e", e);
-            console.log(e.path[2].children[1].currentSrc);
-
-            const imageUrl = e.path[2].children[1].currentSrc;
-            const sliced = imageUrl.slice(22);
-            console.log("sliced", sliced);
-            let indexOfImg = imgArray.findIndex(image => image === sliced);
-            console.log("imgArray", imgArray);
-            console.log("index", indexOfImg);
-            if (indexOfImg >= imgArray.length - 1) indexOfImg = -1;
-            indexOfImg++;
-            return setImg(indexOfImg)
-        }
-
-        function setImg(index) {
-            console.log("index i setImg", index);
-            console.log('setImg');
-            const imagePoppedUp = jsonImages.find(images => images.url === imgArray[index]);
-            console.log("imagePoppedUp", imagePoppedUp);
-            popupImage.setAttribute('src', imgArray[index])
-            descDiv.innerHTML = `<p>${imagePoppedUp.description}</p>`
-        }
-
-        function showText(id) {
-            console.log(jsonImages);
-            console.log(id);
-            const imgObj = jsonImages.find(image => image.id === id);
-            console.log("imgObj", imgObj);
-            return descDiv.innerHTML = `<p>${imgObj.description}</p>`
+        const setImg = (index) => {
+            popupImage.setAttribute('src', jsonImages[index].url)
+            descDiv.innerHTML = `<p>${jsonImages[index].description}</p>`
         }
     })
